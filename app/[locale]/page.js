c2494cbeb9f278a47f222e329d0fe7c113be268c
Senthil-Bala <senthilkumarbala100@gmail.com>
2024-02-26 13:28:07 +0530

    langauge added

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -13,7 +13,7 @@ export default function Home() {
         </Link>{" "}
         |{" "}
         <Link href="/" locale="fi">
-          In Finnish
+        தமிழ்
         </Link>
         <br />
         <br />
